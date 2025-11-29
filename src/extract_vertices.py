966042a8eb6d8ee8464a21af05f42825abbc7d7b
utra-voye <ultravoye@gmail.com>
2025-11-29 19:32:48 +0100

    Extract RGB Color for Every Vertex

--- src/extract_vertices.py
+++ src/extract_vertices.py
@@ -1,13 +1,61 @@
 import bpy
 import csv
 import os
+import random
 from os.path import dirname, join
 
-obj_name = "Cube"
-obj = bpy.data.objects.get(obj_name)
 
-mesh = obj.data
-world_matrix = obj.matrix_world
+KEEP_PROB = 0.00025
+
+
+def get_image_from_object(obj):
+    mat = obj.active_material
+    if not mat or not mat.use_nodes:
+        return None
+
+    for node in mat.node_tree.nodes:
+        if node.type == 'TEX_IMAGE' and node.image is not None:
+            image = node.image
+
+            src_path = bpy.path.abspath(image.filepath)
+            filename = os.path.basename(src_path)
+            dst_path = os.path.join(output_dir, filename)
+            image.save(filepath=dst_path)
+
+            return image
+    return None
+
+
+def sample_image_pixel(img, u, v):
+    u = max(0.0, min(1.0, u))
+    v = max(0.0, min(1.0, v))
+
+    width, height = img.size
+    x = int(u * (width  - 1))
+    y = int(v * (height - 1))
+
+    index = (y * width + x) * 4
+    pixels = img.pixels
+
+    r = pixels[index + 0]
+    g = pixels[index + 1]
+    b = pixels[index + 2]
+
+    return r, g, b
+
+
+root_name = "HG"
+
+root_obj = bpy.data.objects.get(root_name)
+if root_obj is None:
+    raise ValueError(f"Could not find object: '{root_name}'")
+
+if root_obj.type == 'MESH':
+    mesh_objects = [root_obj]
+else:
+    mesh_objects = [child for child in root_obj.children_recursive if child.type == 'MESH']
+
+depsgraph = bpy.context.evaluated_depsgraph_get()
 
 output_dir = bpy.path.abspath("//dummy")
 output_dir = join(dirname(dirname(output_dir)), "output")
@@ -17,19 +65,45 @@ filepath = os.path.join(output_dir, "vertices.csv")
 
 with open(filepath, mode="w", newline="", encoding="utf-8") as csv_file:
     writer = csv.writer(csv_file)
-    writer.writerow(["face_index", "vert_index", "x", "y", "z"])
+    writer.writerow(["object_name", "face_index", "vert_index", "x", "y", "z", "r", "g", "b"])
 
-    for poly in mesh.polygons:
-        for vert_index in poly.vertices:
-            v = mesh.vertices[vert_index]
-            
-            co_world = world_matrix @ v.co
-            writer.writerow([
-                poly.index,
-                vert_index,
-                co_world.x,
-                co_world.y,
-                co_world.z
-            ])
+    for obj in mesh_objects:
+        obj_eval = obj.evaluated_get(depsgraph)
+        mesh = obj_eval.to_mesh()
+        world_matrix = obj.matrix_world
+
+        if not mesh.uv_layers:
+            obj_eval.to_mesh_clear()
+            continue
+
+        uv_layer = mesh.uv_layers.active
+        img = get_image_from_object(obj)
+
+        for poly in mesh.polygons:
+            for loop_index in poly.loop_indices:
+                if random.random() > KEEP_PROB:
+                    continue
+                loop = mesh.loops[loop_index]
+                vert_index = loop.vertex_index
+                v = mesh.vertices[vert_index]
+
+                co_world = world_matrix @ v.co
+
+                uv = uv_layer.data[loop_index].uv
+                u, v_uv = uv.x, uv.y
+
+                r, g, b = sample_image_pixel(img, u, v_uv)
+
+                writer.writerow([
+                    obj.name,
+                    poly.index,
+                    vert_index,
+                    co_world.x,
+                    co_world.y,
+                    co_world.z,
+                    r, g, b
+                ])
+
+        obj_eval.to_mesh_clear()
 
 print(f"CSV saved: {filepath}")
